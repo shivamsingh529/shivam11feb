77d568545133dfdb1a097dcc19365bb58421eacb
Archita <archita1335.be22@chitkara.edu.in>
2025-02-11 15:03:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>welcome to Ec2 running instance....</h1>
+        <h2>Launch...!</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
